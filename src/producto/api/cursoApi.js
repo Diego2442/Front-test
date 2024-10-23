53aeca75126cb3db2a1646846325cbfee255a40f
unknown <u20152139849@usco.edu.co>
2024-10-22 21:00:24 -0500

    api-link

--- src/producto/api/cursoApi.js
+++ src/producto/api/cursoApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const productoApi = axios.create({
-    baseURL: import.meta.env.VITE_API_URL_PRODUCTO
+    //baseURL: import.meta.env.VITE_API_URL_PRODUCTO
+    baseURL: `https://backend-test-pa4s.onrender.com/api/product/`
 })
 
 export default productoApi
